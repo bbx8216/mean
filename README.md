d8eee9b75c121b9c7c8cf610042515fe729d68ea
bbx8216 <bbx8216@gmail.com>
2022-01-04 02:05:21 +0900

    hello-bb8

--- README.md
+++ README.md
@@ -1,4 +1,2 @@
-# Hello Wrold
-이 저장소의 코드는
-[http://a-mean-blog.com/ko/blog/Node-JS-첫걸음/Hello-World](http://a-mean-blog.com/ko/blog/Node-JS-첫걸음/Hello-World)에서 작성되었습니다.
-<br>
+# Hello World
+!! 공부공부 !!
--- index.js
+++ index.js
@@ -1,7 +1,18 @@
+// index.js
+
 var express = require('express');
 var app = express();
 
+app.set('view engine','ejs'); // ejs 사용을 위해 express의 view engine에 ejs 를 set하는 코드
 app.use(express.static(__dirname + '/public'));
+
+app.get('/hello', function(req,res){ // query를 통해서 이름을 받는 코드
+  res.render('hello', {name:req.query.nameQuery});
+});
+
+app.get('/hello/:nameParam', function(req,res){ // route parameter를 통해 이름을 받는 코드
+  res.render('hello', {name:req.params.nameParam});
+});
 
 var port = 3000;
 app.listen(port, function(){
